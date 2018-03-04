19f7d1675181e0901c7bb3fdb161eb4db676e413
Pioverpie <pioverpie@gmail.com>
2018-03-04 09:27:32 -0500

    Removed quotes

--- compileScript.bat
+++ compileScript.bat
@@ -4,10 +4,10 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 call "C:\Program Files\Microsoft Visual Studio 2008\VC\vcvarsall.bat" x86_amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
 cd "%home%"
-echo "Starting Conversion"
+echo Starting Conversion
 set tbc=%~f1
 java Main "%~f1"
-echo "Conversion Complete"
+echo Conversion Complete
 cl "%tbc:~0,-4%.c"
 rem del "%tbc:~0,-4%.c"
 del "%tbc:~0,-4%.obj"
